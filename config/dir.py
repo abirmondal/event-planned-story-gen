cdc1929eb76893c4f07ea1908b411732e0d7827c
Abir Mondal <abir22197072@gmail.com>
2025-08-13 11:00:26 +0530

    Update config dir

--- config/dir.py
+++ config/dir.py
@@ -4,16 +4,10 @@
 This module defines directory paths used in the project.
 """
 
-import os
 from pathlib import Path
-from dotenv import load_dotenv
 
-# This line finds and loads the key-value pairs from your .env file
-load_dotenv()
-
-# Get BASE_DIR from the environment; default to the current directory if not found.
-# The value is immediately converted to a Path object for consistency.
-BASE_DIR = Path(os.environ.get("BASE_DIR", Path.cwd()))
+# Define the base directory as the parent directory of this file's parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Directory for data files
 DATA_DIR = BASE_DIR / "data"
@@ -25,3 +19,18 @@ PROCESSED_DATA_DIR = DATA_DIR / "processed"
 RAW_DATA_DIR = DATA_DIR / "raw"
 # Directory for graph data files
 GRAPH_DATA_DIR = DATA_DIR / "graph"
+
+def update_directories(base_dir: str):
+    """
+    Update the directory paths based on the current environment.
+
+    Args:
+        base_dir (str): The base directory path.
+    """
+    global BASE_DIR, DATA_DIR, ROCSTORIES_DIR, PROCESSED_DATA_DIR, RAW_DATA_DIR, GRAPH_DATA_DIR
+    BASE_DIR = Path(base_dir).resolve()
+    DATA_DIR = BASE_DIR / "data"
+    ROCSTORIES_DIR = DATA_DIR / "processed" / "rocstories_name_replaced"
+    PROCESSED_DATA_DIR = DATA_DIR / "processed"
+    RAW_DATA_DIR = DATA_DIR / "raw"
+    GRAPH_DATA_DIR = DATA_DIR / "graph"
